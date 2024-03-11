e94cf71392318537b92e689d3e19f5f383ee0544
marleen.koberski <marleen.koberski@C11-WIED8XE522Y.dir.svc.accenture.com>
2024-03-11 18:00:33 +0100

    DEV Checklists

--- Install/Sql/MySQL/Migrations/1.0/20240305_1713_1_new_checklists.sql
+++ Install/Sql/MySQL/Migrations/1.0/20240305_1713_1_new_checklists.sql
@@ -1,24 +1,20 @@
-CREATE TABLE IF NOT EXISTS `checklist` (
+CREATE TABLE `checklist` (
   `id` int NOT NULL AUTO_INCREMENT,
   `created_on` datetime NOT NULL DEFAULT (now()),
   `modified_on` datetime NOT NULL DEFAULT (now()),
   `created_by_user_oid` binary(16) NOT NULL,
   `modified_by_user_oid` binary(16) NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `operation_id` int DEFAULT NULL,
   `description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   `component_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `Name unique` (`name`) USING BTREE,
-  KEY `component_id` (`component_id`)
+  KEY `component_id` (`component_id`),
+  CONSTRAINT `checklist_ibfk_1` FOREIGN KEY (`component_id`) REFERENCES `component` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `checklist_data`
---
-
-CREATE TABLE IF NOT EXISTS `checklist_data` (
+CREATE TABLE `checklist_data` (
   `id` int NOT NULL AUTO_INCREMENT,
   `created_on` datetime NOT NULL DEFAULT (now()),
   `modified_on` datetime NOT NULL DEFAULT (now()),
@@ -29,16 +25,11 @@ CREATE TABLE IF NOT EXISTS `checklist_data` (
   `server_component_log_id` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   KEY `checklist_version_id` (`checklist_version_id`),
-  KEY `server_id` (`server_id`)
+  CONSTRAINT `checklist_data_ibfk_1` FOREIGN KEY (`checklist_version_id`) REFERENCES `checklist_version` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `checklist_version`
---
-
-CREATE TABLE IF NOT EXISTS `checklist_version` (
+CREATE TABLE `checklist_version` (
   `id` int NOT NULL AUTO_INCREMENT,
   `created_on` datetime NOT NULL DEFAULT (now()),
   `modified_on` datetime NOT NULL DEFAULT (now()),
@@ -47,32 +38,19 @@ CREATE TABLE IF NOT EXISTS `checklist_version` (
   `version` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `checklist_id` int NOT NULL,
   `form_definition` longtext NOT NULL,
-  `component_id` int DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `Name unique` (`version`) USING BTREE,
-  KEY `checklist_id` (`checklist_id`)
+  UNIQUE KEY `Name unique` (`version`,`checklist_id`) USING BTREE,
+  KEY `checklist_id` (`checklist_id`),
+  CONSTRAINT `checklist_version_ibfk_1` FOREIGN KEY (`checklist_id`) REFERENCES `checklist` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `checklist`
---
-ALTER TABLE `checklist`
-  ADD CONSTRAINT `checklist_ibfk_1` FOREIGN KEY (`component_id`) REFERENCES `component` (`id`);
-
---
--- Constraints for table `checklist_data`
---
-ALTER TABLE `checklist_data`
-  ADD CONSTRAINT `checklist_data_ibfk_1` FOREIGN KEY (`checklist_version_id`) REFERENCES `checklist_version` (`id`),
-  ADD CONSTRAINT `checklist_data_ibfk_2` FOREIGN KEY (`server_id`) REFERENCES `server` (`id`);
-
---
--- Constraints for table `checklist_version`
---
-ALTER TABLE `checklist_version`
-  ADD CONSTRAINT `checklist_version_ibfk_1` FOREIGN KEY (`checklist_id`) REFERENCES `checklist` (`id`);
-COMMIT;
+CREATE TABLE `operation` (
+  `created_on` datetime NOT NULL,
+  `modified_on` datetime NOT NULL,
+  `created_by_user_oid` binary(16) NOT NULL,
+  `modified_by_user_oid` binary(16) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
